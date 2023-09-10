c398aad145fe5afe50bcc52be89e8f6f69621ad8
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-09-10 00:50:33 -0400

    validates author so far

--- server/models.py
+++ server/models.py
@@ -2,22 +2,36 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
+
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
+    # Add validations and constraints
 
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, nullable=False, unique=True)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validates_name(self, key, value):
+        if value == "":
+            raise ValueError("Must have a name")
+        return value
+
+    @validates('phone_number')
+    def validate_author_phonenumber(self, key, value):
+        if len(value) != 10:
+            raise ValueError("phone number must be 10 digits")
+        return value
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
+
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
+    # Add validations and constraints
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -26,7 +40,6 @@ class Post(db.Model):
     summary = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
-
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
